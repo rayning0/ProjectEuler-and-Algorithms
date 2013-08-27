8396d2090a59a66fec64be57ae925715e71447b6
Raymond Gan <rayning@yahoo.com>
2013-08-27 09:52:20 -0700

    #4 Largest palindrome

---
+++ euler/004palindrome.rb
@@ -0,0 +1,19 @@
+# http://projecteuler.net/problem=4
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+def is_palindrome?(n)
+  n.to_s == n.to_s.reverse
+end
+
+palindromes = []
+
+999.downto(900) do |x|    # don't need to look at numbers below 900
+  999.downto(900) do |y|
+    if is_palindrome?(x * y)
+      puts "#{x * y} = #{x} * #{y}"
+      palindromes << x * y
+    end
+  end
+end
+
+puts "Largest palindrome = #{palindromes.max}"
