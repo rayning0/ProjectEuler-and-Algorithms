5b151d44794f7b2d449f4859eb0f23b0b03b0459
Raymond Gan <rayning@yahoo.com>
2013-08-27 13:41:30 -0700

    7. comparing 2 ways to solve 10,001st prime problem

--- euler/007_10001st_prime.rb
+++ euler/007_10001st_prime.rb
@@ -1,6 +1,7 @@
 # http://projecteuler.net/problem=7
 # What is the 10001st prime number?
 
+# This method takes only 1 second!
 #  ------Fast way to check if number is prime------------
 def is_prime?(num)
   return false if num <= 1
@@ -21,8 +22,8 @@ def is_prime?(num)
   true
 end
 
-primecount = 0
 i = 2
+primecount = 0
 
 while true
   primecount += 1 if is_prime?(i)
@@ -32,3 +33,45 @@ while true
   end
   i += 1
 end
+
+=begin
+
+# Alternate slower way: Takes 12 seconds.
+# Build array of primes.
+# All primes, besides 2 or 3, are of form 6k + 1 or 6k - 1. This excludes all multiples of 2 or 3.
+
+primes = [2, 3] # start with 1st 2 primes
+k = 1
+
+while true
+  prime = true
+
+  primes.each do |p|  # is 6k - 1 divisible by each prime found so far?
+    if (6 * k - 1) % p == 0
+      prime = false 
+      break
+    end
+  end
+
+  primes << 6 * k - 1 if prime  # we found a new prime
+
+  break if primes.count == 10001
+
+  prime = true
+
+  primes.each do |p|
+    if (6 * k + 1) % p == 0 # is 6k + 1 divisible by each prime found so far?
+      prime = false
+      break
+    end
+  end
+
+  primes << 6 * k + 1 if prime    # we found a new prime
+
+  break if primes.count == 10001
+  k += 1
+end
+
+puts "10,001st prime = #{primes[-1]}"
+
+=end
