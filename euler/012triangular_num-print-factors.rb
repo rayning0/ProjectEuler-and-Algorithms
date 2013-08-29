32e8019e575bdaf56ff219311e3d3ccabb716069
Raymond Gan <rayning@yahoo.com>
2013-08-29 13:37:36 -0700

    Added step-by-step algorithm example

--- euler/012triangular_num-print-factors.rb
+++ euler/012triangular_num-print-factors.rb
@@ -43,3 +43,55 @@ while true
   end
   i += 1
 end
+
+=begin  [This algorithm, step by step, for number 36]
+
+Answer = [1, 2, 3, 4, 6, 9, 12, 18, 36]
+
+n = 36
+f = 2
+
+36 % 2 = 0    is f*f <= n?
+=> 2, 36/2 = 18  
+n = 18
+18 % 2 = 0    is f*f <= n?
+=> 2, 18/2 = 9  
+n = 9
+9 % 2 != 0 F   or is f*f <= n? 
+
+f: 2 += 1 = 3
+noriginal % f = 0?  (36 % 3) if n < noriginal
+=> 3, 36/3 = 12
+
+9 % 3 = 0   is f*f <= n?
+=> 3, 9/3 = 3
+n = 3
+3 % 2       is f*f <= n? F
+
+f: 3 += 1 = 4
+
+n = noriginal = 36
+36 % 4 = 0  is f*f <= n?
+=> 4, 36/4 = 9
+n = 9
+9 % 4       is f*f <= n? F
+
+f: 4 += 1 = 5
+
+n = original 36
+36 % 5 !=0 F or is f*f <= n?
+
+f: 5 += 1 = 6
+
+36 % 6 = 0    is f*f <= n?
+=> 6, 36/6 = 6
+n = 6
+6 % 6 = 0   or  is f*f <= n? F
+
+f: 6 += 1 = 7
+n = original 36
+is f * f <= noriginal? F
+
+STOP
+
+=end
