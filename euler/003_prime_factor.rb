bb1498ef45724b7357a44308e78fa9fe9dee60ec
Raymond Gan <rayning@yahoo.com>
2017-04-01 04:48:58 -0700

    Rewrite 003_prime_factor to just return largest prime factor

--- euler/003_prime_factor.rb
+++ euler/003_prime_factor.rb
@@ -1,26 +1,22 @@
 # http://projecteuler.net/problem=3
 
-puts "Enter integer. I'll tell you greatest prime factor:"
-n = gets.chomp.to_i
-puts
-
-f = 2
-factors = []
-while f * f <= n    # only need to check up to sqrt(n)
-  if n % f == 0
-    factors << f
-    n = n / f       # key line! After you find a factor, only check quotient after that!
-  else
-    f += 1
+def largest_prime_factor(n)
+  #factors = []
+  f = 2
+  while f * f <= n
+    if n % f == 0
+      #factors << f
+      n /= f
+    else
+      f += 1
+    end
   end
+  # factors << n
+  # factors
+  n
 end
 
-factors << n
-
-print "Prime factors = #{factors}\n"
-puts "Greatest prime factor = #{factors[-1]}"
-
-=begin  
+=begin
 # This method takes < 1 second!
 #  ------Fast way to check if number is prime------------
 
@@ -33,11 +29,11 @@ def is_prime?(num)
 
   j = 5
   w = 2
-  
+
   while j * j <= num    # only check j up to sqrt(num), since factors invert order afterwards
-    return false if num % j == 0  
+    return false if num % j == 0
     j += w              # Only checks odd numbers. First checks j + 2
-    w = 6 - w           # Don't check multiples of 3. So then checks j + 4. 
+    w = 6 - w           # Don't check multiples of 3. So then checks j + 4.
                         # Alternates + 2, + 4, + 2, etc.
   end
 
